12875113e987cbc076247b538f90d0c03978755b
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2023-11-11 00:00:26 +0100

    added links for API

--- src/components/services/themoviedb-API.js
+++ src/components/services/themoviedb-API.js
@@ -23,7 +23,17 @@ async function fetchData() {
 export default fetchData();
 
 // /trending/get-trending список самых популярных фильмов на сегодня для создания коллекции на главной странице.
+// https://api.themoviedb.org/3/trending/movie/day?language=en-US
+// https://api.themoviedb.org/3/trending/movie/day?language=en-US
 // /search/search-movies поиск кинофильма по ключевому слову на странице фильмов.
+// https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1
+// https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1
 // /movies/get-movie-details запрос полной информации о фильме для страницы кинофильма.
+// https://api.themoviedb.org/3/movie/movie_id?language=en-US
+// https://api.themoviedb.org/3/movie/movie_id?language=en-US
 // /movies/get-movie-credits запрос информации о актёрском составе для страницы кинофильма.
+// https://api.themoviedb.org/3/movie/movie_id/credits?language=en-US
+// https://api.themoviedb.org/3/movie/movie_id/credits?language=en-US
 // /movies/get-movie-reviews запрос обзоров для страницы кинофильма.
+// https://api.themoviedb.org/3/movie/movie_id/reviews?language=en-US&page=1
+// https://api.themoviedb.org/3/movie/movie_id/reviews?language=en-US&page=1
