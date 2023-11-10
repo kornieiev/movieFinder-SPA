1fe0a7c1fd5dd6ebf61d979b105735169ae2ee1e
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2023-11-11 00:09:46 +0100

    Update themoviedb-API.js
    add obj with URL

--- src/components/services/themoviedb-API.js
+++ src/components/services/themoviedb-API.js
@@ -22,18 +22,31 @@ async function fetchData() {
 }
 export default fetchData();
 
+// тут надо будет сделать заготовки с именами под нужный запрос и передавать имя этого запроса в функцию вызова фетч
+
 // /trending/get-trending список самых популярных фильмов на сегодня для создания коллекции на главной странице.
 // https://api.themoviedb.org/3/trending/movie/day?language=en-US
-// https://api.themoviedb.org/3/trending/movie/day?language=en-US
+
 // /search/search-movies поиск кинофильма по ключевому слову на странице фильмов.
 // https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1
-// https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1
+
 // /movies/get-movie-details запрос полной информации о фильме для страницы кинофильма.
 // https://api.themoviedb.org/3/movie/movie_id?language=en-US
-// https://api.themoviedb.org/3/movie/movie_id?language=en-US
+
 // /movies/get-movie-credits запрос информации о актёрском составе для страницы кинофильма.
 // https://api.themoviedb.org/3/movie/movie_id/credits?language=en-US
-// https://api.themoviedb.org/3/movie/movie_id/credits?language=en-US
+
 // /movies/get-movie-reviews запрос обзоров для страницы кинофильма.
 // https://api.themoviedb.org/3/movie/movie_id/reviews?language=en-US&page=1
-// https://api.themoviedb.org/3/movie/movie_id/reviews?language=en-US&page=1
+
+const URL = {
+  trendingMovie:
+    'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+  searchMovie:
+    'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1',
+  detailsMovie: 'https://api.themoviedb.org/3/movie/movie_id?language=en-US',
+  creditsMovie:
+    'https://api.themoviedb.org/3/movie/movie_id/credits?language=en-US',
+  reviewsMovie:
+    'https://api.themoviedb.org/3/movie/movie_id/reviews?language=en-US&page=1',
+};
