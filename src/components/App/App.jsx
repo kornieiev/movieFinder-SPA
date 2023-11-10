87da83e0b5d5263dc625086b26772f5cc5f242de
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2023-11-10 11:53:29 +0100

    add BrowserRouter

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import Header from '../Header/Header';
+
+export const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      React homework template
+    </div>
+  );
+};
---
+++ src/components/Header/Headre.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HeaderWrap = styled.header`
+  display: flex;
+  width: 100%;
+  border-bottom: 1px solid grey;
+`;
+
+export const Title = styled.h3`
+  margin-left: 20px;
+`;
